3aeb9972c915d06bfc0d51d44334a8c50ed7b10e
pavlenex <pavlenex@users.noreply.github.com>
2020-05-31 15:16:49 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.8db0866341d356c5b61e73e5fa483338.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d286f3d611154780cb9b6a2d65f2f378",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c4875e414a7b1c79e6f",
+    "url": "/static/css/main.4fa132b4.chunk.css"
+  },
+  {
+    "revision": "9cfe4f2d0520a75029d6",
+    "url": "/static/js/2.7392cb79.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.7392cb79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4875e414a7b1c79e6f",
+    "url": "/static/js/main.f7565018.chunk.js"
+  },
+  {
+    "revision": "8046168c7d1eb6a0a47f",
+    "url": "/static/js/runtime-main.156ac236.js"
+  },
+  {
+    "revision": "a51d790b3fd71b506e6766701e1e3bd1",
+    "url": "/static/media/btcpay-directory-logo-white.a51d790b.png"
+  },
+  {
+    "revision": "6e3359f9b05a554e80377d08e7ca1e03",
+    "url": "/static/media/btcpay-directory-logo.6e3359f9.svg"
+  },
+  {
+    "revision": "7fe50f25c11d11f5833b33d03a17fa04",
+    "url": "/static/media/moonFilled.7fe50f25.svg"
+  },
+  {
+    "revision": "eae3aacdfedd630b137d25a46201d928",
+    "url": "/static/media/sunFilled2.eae3aacd.svg"
+  }
+]);
